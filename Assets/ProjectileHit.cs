25d22a5acd17f82736c1fe9f8f4ed51ba93fc90c
JoaoLReis <jype_13@hotmail.com>
2013-08-04 21:35:31 +0100

    bullets collision
    bullet collide with enemy and enemy dies

--- Assets/ProjectileHit.cs
+++ Assets/ProjectileHit.cs
@@ -3,14 +3,18 @@ using System.Collections;
 
 public class ProjectileHit : MonoBehaviour {
 	
-	
+	private BulletStats bulletstats;
+	private BasicShipStats shipstats;
 	
 	void OnTriggerEnter (Collider other) {
-		if(other.CompareTag("PlayerShot")){
-			BulletStats comp = other.GetComponent<BulletStats>();
-			BasicShipStats stats = GetComponentInChildren<BasicShipStats>();
-			stats.decreaseHealth(comp.getDamage() * (1 - (stats.getArmor() * comp.getArmorPen())));
+		
+		if(other.CompareTag("Enemy")){
+			bulletstats = GetComponent<BulletStats>();
+			shipstats = other.GetComponent<BasicShipStats>();
+			shipstats.decreaseHealth(bulletstats.getDamage() * (1 - (shipstats.getArmor() * bulletstats.getArmorPen())));
+			Destroy(gameObject);
 		}
 	}
+	
 	
 }
