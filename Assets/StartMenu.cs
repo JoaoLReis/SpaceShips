108b8a4cfe0895a5aa94e40da4eb8a17e258c366
JoaoLReis <jype_13@hotmail.com>
2013-09-06 21:57:22 +0100

    Start menu

---
+++ Assets/StartMenu.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour {
+
+	public GUISkin _menuskin;
+	public GUIStyle _title;
+	private Rect _creditwindow = new Rect((Screen.width*4)/5, Screen.height/10, Screen.width/5, (Screen.height*4)/5);
+	private bool _showcredits = false;
+	public Texture2D _mute;
+	public Texture2D _unmute;
+	public Texture2D _sound;
+	public Texture2D _nosound;
+	
+	
+	void OnGUI(){
+		
+		//The title box
+		GUI.Box(new Rect(Screen.width/4, Screen.height/5, Screen.width/2, Screen.width/10), "SPACE SHIPS", _title);
+		
+		//The "Game developed by" box
+		GUI.Box(new Rect(0, (Screen.height*14)/15, Screen.width/4, Screen.height/15), "Game developed By", _menuskin.box);
+		
+		//The mute sound and music button
+		if(GUI.Button(new Rect((Screen.width*8/10), 0, Screen.width/20, Screen.height/20), new GUIContent(_unmute, "Click to mute"), _menuskin.button))
+			
+		
+		//if(GUI.Button(new Rect((Screen.width*17/20), 0, Screen.width/20, Screen.height/20), new GUIContent(_sound, "Click to mute"), _menuskin.button))
+			
+		
+		
+		
+		
+		
+		//Start button
+		if(GUI.Button(new Rect((Screen.width/2) - Screen.height/10, (Screen.height/2) - Screen.height/10, Screen.width/10, Screen.height/10), new GUIContent("Start", "Click to start"), _menuskin.button)){
+			Application.LoadLevel("SpaceShips");
+			
+		}
+		
+		//Options Button
+		GUI.Button(new Rect((Screen.width/2) - Screen.height/10, (Screen.height/2), Screen.width/10, Screen.height/10), new GUIContent("Options", "Click to mute, change volume settings or game quality"), _menuskin.button);
+		
+		
+		//Credits Window
+		if(GUI.Button(new Rect((Screen.width/2) - Screen.height/10, (Screen.height/2) + Screen.height/10, Screen.width/10, Screen.height/10), new GUIContent("Credits", "Click to view the credits"), _menuskin.button)){
+			if(_showcredits == true)
+				_showcredits = false;
+			else _showcredits = true;
+
+		}
+		
+		if(_showcredits)
+			_creditwindow = GUI.Window(1, _creditwindow, doMyWindow, "Credits\n\n Music\n SoundFx\n Special Thanks to\n", _menuskin.window);
+				
+		GUI.Label (new Rect (Input.mousePosition.x + 10,Screen.height-(Input.mousePosition.y) - 10,400,20), GUI.tooltip, _menuskin.label);
+	}
+	
+	void doMyWindow (int windowid) {
+		Debug.Log("doMyWindow");
+	}
+}
